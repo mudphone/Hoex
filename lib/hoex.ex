5694d71d700185e98c1225bb70b56ae68ca06353
Kyle Oba <koba@mudphone.com>
2015-05-02 04:54:56 -1000

    Add curry.

--- lib/hoex.ex
+++ lib/hoex.ex
@@ -1,2 +1,24 @@
 defmodule Hoex do
+
+  def arity(f) when is_function(f), do: arity(f, 0)
+  defp arity(f, n) when is_function(f, n), do: n
+  defp arity(f, n), do: arity(f, n+1)
+
+  defp curryize(f, 0, args) do
+    apply(f, args)
+  end
+
+  defp curryize(f, n, args) do
+    fn x ->
+      curryize(f, n-1, args ++ [x])
+    end
+  end
+
+  def curry(f) do
+    case n = arity(f) do
+      0 -> f
+      _ -> curryize(f, n, [])
+    end
+  end
+
 end
--- test/hoex_test.exs
+++ test/hoex_test.exs
@@ -1,7 +1,29 @@
 defmodule HoexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defmodule Mathy do
+    def no_op, do: :ok
+    def div(a, b, c), do: a / b / c
   end
+
+  test "arity calculation" do
+    assert Hoex.arity(&AnyModule.any_name/4) == 4
+    assert Hoex.arity(&AnyModule.any_name/1) == 1
+    assert Hoex.arity(&AnyModule.any_name/0) == 0
+  end
+
+  test "curry of a arity 3 function" do
+    f = Hoex.curry(&Mathy.div/3)
+    f2 = f.(10)
+    f3 = f2.(5)
+    assert f.(10).(5).(2) == 1.0
+    assert f2.(2).(1) == 5.0
+    assert f3.(2) == 1.0
+  end
+
+  test "curry of a arity 0 function" do
+    f = Hoex.curry(&Mathy.no_op/0)
+    assert f.() == Mathy.no_op
+  end
+
 end
